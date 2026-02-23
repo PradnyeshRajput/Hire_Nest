38e6c30be56188595d6a06d95e2b4726839ff3c3
Sahil kadam <kadamsahil642@gmail.com>
2026-02-23 18:12:48 +0530

    Add modal

--- src/main/java/com/example/demo/HireNestApplication.java
+++ src/main/java/com/example/demo/HireNestApplication.java
@@ -9,11 +9,7 @@ public class HireNestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HireNestApplication.class, args);
 		System.out.println("hi");
-		System.out.println("hello");
-		System.out.println("ok");
-		System.out.println("pradnyesh66");
-		System.out.println("Sahil33");
-		System.out.println("Done");
+		
 	}
 
 }
